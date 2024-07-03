5551d19eedd3a072abc9bf6622bb79036889e246
Mikka Hawari Dhiyaulhaq <mikkadhiyaulhaq@gmail.com>
2024-07-03 18:09:55 +0700

    add image upload on production

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -72,13 +72,23 @@ class ProductionController extends Controller
      */
     public function update(Request $request, Production $production)
     {
+        $request->validate([
+            'in_image' => 'required|image|mimes:jpeg,png,jpg,gif',
+        ]);
+        $folderPath = 'production/img/'.$request->in_preorder.'/';
+        $imageName = 'PO'.$request->in_preorder.'_'.time().'.'.$request->in_image->extension();
+
         $prod = Production::create([
             'preorder' => $request->in_preorder,
             'taskst' => $request->in_taskst,
             'note' => $request->in_note,
             'datetime' => Carbon::now(),
             'staff' => Auth::id(),
+            'image' => $folderPath.$imageName,
         ]);
+        
+        $request->in_image->move(public_path($folderPath), $imageName);
+
         $ProdId = $request->in_id;
         $archv = Production::where('id',$ProdId)->update(['status' => 0]);
         
--- resources/views/production/edit.blade.php
+++ resources/views/production/edit.blade.php
@@ -21,7 +21,7 @@
     @endif
 
     <div class="card-body row g-3 form-group">
-        <form action="{{ route('prod.update',$row->id) }}" method="POST">
+        <form action="{{ route('prod.update',$row->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method("PUT")
             <input class="form-control" type="hidden" name="in_id" placeholder="Kata Kunci"
@@ -40,11 +40,16 @@
         <span class="input-group-text">Catatan<span class="text-danger">*</span></span>
         <input class="form-control" type="" name="in_note" placeholder="Catatan" />
     </div>
-    <div class="input-group mb-3 justify-content-center">
-        <button class="btn btn-primary">Simpan</button>
-        <a class="btn btn-danger" href="{{ url()->previous() }}">Kembali</a>
-    </div>
-    </form>
+</div>
+<div class="input-group mb-3">
+    <span class="input-group-text">Upload Gambar<span class="text-danger">*</span></span>
+    <input class="form-control" type="file" name="in_image" />
+</div>
+<div class="input-group mb-3 justify-content-center">
+    <button class="btn btn-primary">Simpan</button>
+    <a class="btn btn-danger" href="{{ url()->previous() }}">Kembali</a>
+</div>
+</form>
 </div>
 </div>
 @endsection
